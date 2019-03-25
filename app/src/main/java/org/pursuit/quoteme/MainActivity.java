adbddc07bba7a7b656921dd2941aa8c0e9e6a0e3
garganjohn <garganjohn@gmail.com>
2019-03-25 14:07:09 -0400

    added necessary classes, acivities and fragments for viewpager setup

--- app/src/main/java/org/pursuit/quoteme/MainActivity.java
+++ app/src/main/java/org/pursuit/quoteme/MainActivity.java
@@ -1,13 +1,19 @@
 package org.pursuit.quoteme;
 
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
+    public static final String TAG = "Main Activity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+         ViewPager viewPager = findViewById(R.id.main_act_viewpager);
+         //viewPager.setAdapter(getSupportFragmentManager(), );
     }
 }
