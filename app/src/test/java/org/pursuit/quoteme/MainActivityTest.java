e126171e8976368ab00e6d0bdcc45c06e5a27ad5
garganjohn <garganjohn@gmail.com>
2019-04-09 19:25:30 -0400

    more string resource cleanup and added j-unit test

--- app/src/test/java/org/pursuit/quoteme/MainActivityTest.java
+++ app/src/test/java/org/pursuit/quoteme/MainActivityTest.java
@@ -1,4 +1,33 @@
 package org.pursuit.quoteme;
 
+import android.support.v4.app.Fragment;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.pursuit.quoteme.fragment.ViewPagerFragment;
+
 public class MainActivityTest {
+    Fragment frag;
+    @Before
+    public void setup() {
+        frag = ViewPagerFragment.getInstance("");
+
+    }
+
+    @Test
+    public void viewPagerFragment_Name_With_Null() {
+//       String testString = null;
+//        Fragment expected = ViewPagerFragment.getInstance(testString);
+        Fragment actual = frag;
+
+        Assert.assertNotNull(actual);
+    }
+
+
+    @After
+    public void tearDown() {
+
+    }
 }
